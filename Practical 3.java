5e84c0407994bc8a544dba052d73b394fcbfc6fc
harshitn2002 <84696979+harshitn2002@users.noreply.github.com>
2022-04-07 22:32:11 +0530

    Create Practical 3.java

---
+++ Practical 3.java
@@ -0,0 +1,11 @@
+class SUM
+{
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+
+		System.out.println("Sum of 2 numbers using command Line Arguments: "+(a+b));
+	}
+	
+}
